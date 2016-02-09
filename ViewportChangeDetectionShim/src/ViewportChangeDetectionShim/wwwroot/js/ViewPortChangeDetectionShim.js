0d34897b2c9a10dc07693615e13af32a0cd3c2fe
Jeff Hughes <me@jeffhughes.com>
2016-02-09 01:35:07 -0600

    created events

--- ViewportChangeDetectionShim/src/ViewportChangeDetectionShim/wwwroot/js/ViewPortChangeDetectionShim.js
+++ ViewportChangeDetectionShim/src/ViewportChangeDetectionShim/wwwroot/js/ViewPortChangeDetectionShim.js
@@ -1,34 +1,19 @@
-﻿var lastHeight, lastWidth, lastVerticalOrientation, lastScrollPosition = 0;
+﻿var CheckViewportLastHeight, CheckViewportLastWidth,
+    CheckViewportLastVerticalOrientation, CheckViewportLastScrollPosition = 0;
 
 var CheckViewportIntervalHandle = null;
 
-function CheckViewport(interval,
-    HeightChangeCallBackFunction,
-    WidthChangeCallBackFunction,
-    HeightOrWidthChangeCallBackFunction,
-    OrientationChangeCallBackFunction,
-    ScrollPositionCallBackFunction,
-    AnythingChangedCallBackFunction
-    ) {
-    CheckViewportIntervalHandle = setInterval(CheckViewportTasks,
-        interval,
-        HeightChangeCallBackFunction,
-        WidthChangeCallBackFunction,
-        HeightOrWidthChangeCallBackFunction,
-        OrientationChangeCallBackFunction,
-        ScrollPositionCallBackFunction,
-        AnythingChangedCallBackFunction
-        );
-}
+$(function () {
+    CheckViewportLastHeight = $(window).height();
+    CheckViewportLastWidth = $(window).width();
+    CheckViewportLastVerticalOrientation = (CheckViewportLastHeight > CheckViewportLastWidth);
+    CheckViewportLastScrollPosition = $(window).scrollTop();
 
-function CheckViewportTasks(
-    HeightChangeCallBackFunction,
-    WidthChangeCallBackFunction,
-    HeightOrWidthChangeCallBackFunction,
-    OrientationChangeCallBackFunction,
-    ScrollPositionCallBackFunction,
-    AnythingChangedCallBackFunction) {
-    console.log("---Viewport checked");
+    CheckViewportIntervalHandle = setInterval(CheckViewportTasks, 250);
+});
+
+function CheckViewportTasks() {
+    //console.log("---Viewport checked");
 
     var height = $(window).height();
     var width = $(window).width();
@@ -39,52 +24,49 @@ function CheckViewportTasks(
     var somethingChanged = false;
     var heightWidthChanged = false;
 
-    if (height !== lastHeight) {
+    if (height !== CheckViewportLastHeight) {
         //console.log("Viewport change");
-        //console.log("old height: " + lastHeight + ", width: " + lastWidth);
+        //console.log("old height: " + CheckViewportLastHeight + ", width: " + CheckViewportLastWidth);
         //console.log("new height: " + height + ", width: " + width);
         somethingChanged = true;
         heightWidthChanged = true;
-        if (lastHeight !== 0 && HeightChangeCallBackFunction !== null)
-            HeightChangeCallBackFunction();
+
+        $(document).trigger("HeightChanged");
     }
 
-    if (width !== lastWidth) {
+    if (width !== CheckViewportLastWidth) {
         //console.log("Viewport change");
-        //console.log("old height: " + lastHeight + ", width: " + lastWidth);
+        //console.log("old height: " + CheckViewportLastHeight + ", width: " + CheckViewportLastWidth);
         //console.log("new height: " + height + ", width: " + width);
         somethingChanged = true;
         heightWidthChanged = true;
-        if (lastWidth !== 0 && WidthChangeCallBackFunction !== null)
-            WidthChangeCallBackFunction();
+        $(document).trigger("WidthChanged");
     }
 
-    if (heightWidthChanged && HeightOrWidthChangeCallBackFunction !== null)
-        HeightOrWidthChangeCallBackFunction();
+    if (heightWidthChanged)
+        $(document).trigger("HeightOrWidthChanged");
 
-    if (vorientation !== lastVerticalOrientation) {
+    if (vorientation !== CheckViewportLastVerticalOrientation) {
         //console.log("orientation change");
         //console.log("old orientation: " + verticalOrientation);
         //console.log("new orientation: " + vorientation);
         somethingChanged = true;
-        if (lastVerticalOrientation !== 0 && OrientationChangeCallBackFunction !== null)
-            OrientationChangeCallBackFunction();
+        $(document).trigger("OrientationChanged");
     }
 
-    if (position !== lastScrollPosition) {
+    if (position !== CheckViewportLastScrollPosition) {
         //console.log("position change");
         //console.log("old position: " + scrollPosition);
         //console.log("new position: " + position);
         somethingChanged = true;
-        if (lastScrollPosition !== 0 && ScrollPositionCallBackFunction !== null)
-            ScrollPositionCallBackFunction();
+        $(document).trigger("ScrollPositionChanged");
     }
 
-    lastHeight = height;
-    lastWidth = width;
-    lastVerticalOrientation = vorientation;
-    lastScrollPosition = position;
+    CheckViewportLastHeight = height;
+    CheckViewportLastWidth = width;
+    CheckViewportLastVerticalOrientation = vorientation;
+    CheckViewportLastScrollPosition = position;
 
-    if (somethingChanged && AnythingChangedCallBackFunction !== null)
-        AnythingChangedCallBackFunction();
+    if (somethingChanged)
+        $(document).trigger("ViewportChanged");
 }
